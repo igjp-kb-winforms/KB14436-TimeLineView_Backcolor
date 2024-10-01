bfef49b8b9b037bc5dc411b7c7e2d868a5427b95
mkamiishi <mkamiishi@infragistics.com>
2024-10-01 18:00:55 +0900

    初回コミット
    Initial commit

---
+++ TimeLineView_Backcolor/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace TimeLineView_Backcolor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ultraTimelineView1 = new Infragistics.Win.UltraWinSchedule.UltraTimelineView();
+            this.ultraCalendarInfo1 = new Infragistics.Win.UltraWinSchedule.UltraCalendarInfo(this.components);
+            this.ultraCalendarLook1 = new Infragistics.Win.UltraWinSchedule.UltraCalendarLook(this.components);
+            this.ultraTimelineView2 = new Infragistics.Win.UltraWinSchedule.UltraTimelineView();
+            this.ultraCalendarInfo2 = new Infragistics.Win.UltraWinSchedule.UltraCalendarInfo(this.components);
+            this.ultraCalendarLook2 = new Infragistics.Win.UltraWinSchedule.UltraCalendarLook(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTimelineView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTimelineView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraTimelineView1
+            // 
+            this.ultraTimelineView1.Location = new System.Drawing.Point(13, 13);
+            this.ultraTimelineView1.Name = "ultraTimelineView1";
+            this.ultraTimelineView1.Size = new System.Drawing.Size(1022, 298);
+            this.ultraTimelineView1.TabIndex = 0;
+            // 
+            // ultraCalendarInfo1
+            // 
+            this.ultraCalendarInfo1.DataBindingsForAppointments.BindingContextControl = this;
+            this.ultraCalendarInfo1.DataBindingsForOwners.BindingContextControl = this;
+            // 
+            // ultraTimelineView2
+            // 
+            this.ultraTimelineView2.Location = new System.Drawing.Point(13, 336);
+            this.ultraTimelineView2.Name = "ultraTimelineView2";
+            this.ultraTimelineView2.Size = new System.Drawing.Size(1022, 308);
+            this.ultraTimelineView2.TabIndex = 1;
+            // 
+            // ultraCalendarInfo2
+            // 
+            this.ultraCalendarInfo2.DataBindingsForAppointments.BindingContextControl = this;
+            this.ultraCalendarInfo2.DataBindingsForOwners.BindingContextControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 654);
+            this.Controls.Add(this.ultraTimelineView2);
+            this.Controls.Add(this.ultraTimelineView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTimelineView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraTimelineView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.UltraWinSchedule.UltraTimelineView ultraTimelineView1;
+        private Infragistics.Win.UltraWinSchedule.UltraCalendarInfo ultraCalendarInfo1;
+        private Infragistics.Win.UltraWinSchedule.UltraCalendarLook ultraCalendarLook1;
+        private Infragistics.Win.UltraWinSchedule.UltraTimelineView ultraTimelineView2;
+        private Infragistics.Win.UltraWinSchedule.UltraCalendarInfo ultraCalendarInfo2;
+        private Infragistics.Win.UltraWinSchedule.UltraCalendarLook ultraCalendarLook2;
+    }
+}
+
---
+++ TimeLineView_Backcolor/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Infragistics.Win.UltraWinSchedule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeLineView_Backcolor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ultraTimelineView1の設定
+            ultraTimelineView1.EnsureDateTimeVisible(new DateTime(2024, 1, 1));
+
+            //Timeslot intervalの設定
+            DateInterval dateInterval = new DateInterval(1, DateIntervalUnits.Days);
+            dateInterval.HeaderTextFormat = "MM/dd";
+            this.ultraTimelineView1.AdditionalIntervals.Add(dateInterval); 
+            DateInterval datePInterval = new DateInterval(1, DateIntervalUnits.Days);
+            datePInterval.HeaderTextFormat = "ddd";
+            this.ultraTimelineView1.PrimaryInterval = datePInterval;
+
+            // UltraCalendarInfo を TimeLineView コントロールに指定する
+            this.ultraTimelineView1.CalendarInfo = this.ultraCalendarInfo1;
+            // UltraCalendarLook を TimeLineView コントロールに指定する
+            this.ultraTimelineView1.CalendarLook = this.ultraCalendarLook1;
+
+            //特定の日にちの背景色を変更する
+            ultraCalendarLook1.DaysOfYearLook[(int)YearMonthEnum.January, 5].Appearance.BackColor = Color.Red;
+            ultraCalendarLook1.DaysOfYearLook[(int)YearMonthEnum.January, 10].Appearance.BackColor = Color.Blue;
+
+
+
+            //ultraTimelineView2の設定
+            ultraTimelineView2.EnsureDateTimeVisible(new DateTime(2024, 1, 1));
+
+            //Timeslot intervalの設定
+            DateInterval dateInterval2 = new DateInterval(1, DateIntervalUnits.Days);
+            dateInterval2.HeaderTextFormat = "MM/dd";
+            this.ultraTimelineView2.AdditionalIntervals.Add(dateInterval2); 
+            DateInterval datePInterval2 = new DateInterval(1, DateIntervalUnits.Days);
+            datePInterval2.HeaderTextFormat = "ddd";
+            this.ultraTimelineView2.PrimaryInterval = datePInterval2;
+
+            // UltraCalendarInfo を TimeLineView コントロールに指定する
+            this.ultraTimelineView2.CalendarInfo = this.ultraCalendarInfo2;
+            // UltraCalendarLook を TimeLineView コントロールに指定する
+            this.ultraTimelineView2.CalendarLook = this.ultraCalendarLook2;
+
+            //特定の曜日の背景色を変更する
+            ultraCalendarLook2.DaysOfWeekLook[DayOfWeekEnum.Wednesday].Appearance.BackColor = Color.Green;
+        }
+    }
+}
